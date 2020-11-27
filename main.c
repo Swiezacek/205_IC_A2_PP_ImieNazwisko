84280b19c1fc061d153ca0b31343a78c12f3ef5d
Tymoteusz Jeske-Stock <75068968+Swiezacek@users.noreply.github.com>
2020-11-27 10:59:12 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main() {
     int i = 1;
     float f = 1.5;
     double d = 2.5;
-    char c = "c";
+    char c = 'c';
     printf("Wartosci moich zmiennych i = %i, f = %.2f, d = %.2lf, c = %c\n", i, f, d, c);
     scanf("%i %f %lf %c", &i, &f, &d, &c);
     printf("Wartosci moich zmiennych i = %i, f = %.2f, d = %.2lf, c = %c\n", i, f, d, c);
