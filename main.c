0d674620497e7c18b0ee1e306549d4e45b1bc6d3
Tymoteusz Jeske-Stock <75068968+Swiezacek@users.noreply.github.com>
2020-11-27 10:33:26 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+    float f = 1.5;
+    double d = 2.5;
+    char c = "c";
+    printf("Wartosci moich zmiennych i = %i, f = %.2f, d = %.2lf, c = %c\n", i, f, d, c);
+    scanf("%i %f %lf %c", &i, &f, &d, &c);
+    printf("Wartosci moich zmiennych i = %i, f = %.2f, d = %.2lf, c = %c\n", i, f, d, c);
+    return 0;
+}
